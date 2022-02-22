648157e6e2cda940a958e630ae8913f31f44adff
ThejashwiniKS <100185195+ThejashwiniKS@users.noreply.github.com>
2022-02-22 12:25:31 +0530

    Add files via upload

---
+++ SQL QUERY-CustomerFullStack.sql
@@ -0,0 +1,13 @@
+
+------- Creation Of DataBase-----------
+
+create database db2022
+
+------- DataBase Name(db2022)------------
+
+use db2022 
+
+select * from Customers  
+
+select * from CustomerAddresses
+
